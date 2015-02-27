5b762a67a17c6fc40082111d56f4ba3ab9e7a475
Ludovic LEFEVRE <ludovic.lefevre@atinternet.com>
2015-02-27 12:42:36 +0100

    First commit

---
+++ tasks/escomplex.js
@@ -0,0 +1,54 @@
+/*
+ * grunt-escomplex
+ * 
+ *
+ * Copyright (c) 2015 Ludovic LEFEVRE
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var complexity = require('escomplex-js');
+
+var escomplex = function (grunt) {
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('escomplex', 'Code complexity module Grunt using escomplex.', function () {
+
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function (file) {
+      // Concat specified files.
+      var src = file.src.filter(function (filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function (filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(file.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + file.dest + '" created.');
+    });
+  });
+
+};
+
+module.exports = escomplex
