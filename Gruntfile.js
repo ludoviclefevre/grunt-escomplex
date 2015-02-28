62153ee0a2eacacbf1cd69853444a7c2d9210199
Ludovic LEFEVRE <contact@ludoviclefevre.fr>
2015-02-28 01:17:48 +0100

    Refactoring

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 /*
  * grunt-escomplex
- * 
+ *
  *
  * Copyright (c) 2015 Ludovic LEFEVRE
  * Licensed under the MIT license.
@@ -9,63 +9,59 @@
 'use strict';
 
 module.exports = function (grunt) {
-  // load all npm grunt tasks
-  require('load-grunt-tasks')(grunt);
+    // load all npm grunt tasks
+    require('load-grunt-tasks')(grunt);
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-        reporter: require('jshint-stylish')
-      }
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
-
-    // Configuration to be run (and then tested).
-    escomplex: {
-      default_options: {
-        options: {
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js',
+                '<%= nodeunit.tests %>'
+            ],
+            options: {
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
+            }
         },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
+
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            tests: ['tmp']
         },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+
+        jscs: {
+            src: [
+                '<%= jshint.all %>'
+            ]
+        },
+
+        // Configuration to be run (and then tested).
+        escomplex: {
+            default_options: {
+                options: {},
+                src: [
+                    'tasks/*.js'
+                ]
+            }
+        },
+
+        // Unit tests.
+        nodeunit: {
+            tests: ['test/*_test.js']
         }
-      }
-    },
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
+    });
 
-  });
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    grunt.registerTask('test', ['clean', 'escomplex', 'nodeunit']);
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'escomplex', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'jscs', 'test']);
 
 };
--- tasks/escomplex.js
+++ tasks/escomplex.js
@@ -1,6 +1,6 @@
 /*
  * grunt-escomplex
- * 
+ *
  *
  * Copyright (c) 2015 Ludovic LEFEVRE
  * Licensed under the MIT license.
@@ -8,47 +8,81 @@
 
 'use strict';
 
-var complexity = require('escomplex-js');
+var complexity = require('escomplex-js'),
+    async = require('async'),
+    escomplex = function (grunt) {
+        // Please see the Grunt documentation for more information regarding task
+        // creation: http://gruntjs.com/creating-tasks
 
-var escomplex = function (grunt) {
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+        var task = function () {
 
-  grunt.registerMultiTask('escomplex', 'Code complexity module Grunt using escomplex.', function () {
+            var done = this.async(),
 
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+                complexityOptions = {
+                    newmi: true
+                },
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function (file) {
-      // Concat specified files.
-      var src = file.src.filter(function (filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function (filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+                analyseFile = function (filepath, callback) {
+                    var result = complexity.analyse(filepath, complexityOptions);
+                    console.log(filepath + ': ' + result.maintainability);
+                    //console.log(result);
+                    callback();
+                },
 
-      // Handle options.
-      src += options.punctuation;
+                getFilteredFiles = function (filepath) {
+                    // Warn on and remove invalid source files (if nonull was set).
+                    if (!grunt.file.exists(filepath)) {
+                        grunt.log.warn('Source file "' + filepath + '" not found.');
+                        return false;
+                    } else {
+                        return true;
+                    }
+                },
 
-      // Write the destination file.
-      grunt.file.write(file.dest, src);
+                filter = function (file, callback) {
+                    var sources = file.src.filter(getFilteredFiles);
+                    async.each(sources, analyseFile, callback);
+                };
 
-      // Print a success message.
-      grunt.log.writeln('File "' + file.dest + '" created.');
-    });
-  });
+            async.each(this.files, filter, done);
 
-};
+            /*
+             // Merge task-specific and/or target-specific options with these defaults.
+             var options = this.options({
+             punctuation: '.',
+             separator: ', '
+             });
+
+             // Iterate over all specified file groups.
+             this.files.forEach(function (file) {
+             // Concat specified files.
+             var src = file.src.filter(function (filepath) {
+             // Warn on and remove invalid source files (if nonull was set).
+             if (!grunt.file.exists(filepath)) {
+             grunt.log.warn('Source file "' + filepath + '" not found.');
+             return false;
+             } else {
+             return true;
+             }
+             }).map(function (filepath) {
+             // Read file source.
+             return grunt.file.read(filepath);
+             }).join(grunt.util.normalizelf(options.separator));
+
+             // Handle options.
+             src += options.punctuation;
+
+             // Write the destination file.
+             grunt.file.write(file.dest, src);
+
+             // Print a success message.
+             grunt.log.writeln('File "' + file.dest + '" created.');
+             });
+             */
+        };
+
+        grunt.registerMultiTask('escomplex', 'Code complexity module Grunt using escomplex.', task);
+
+    };
 
 module.exports = escomplex
